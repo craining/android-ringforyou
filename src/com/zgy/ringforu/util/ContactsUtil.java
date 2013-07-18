de189156ff6966a14d7be12bc7513a7ef0b87c5b
unknown <craining@163.com>
2013-07-18 23:19:24 +0800

    bug

--- src/com/zgy/ringforu/util/ContactsUtil.java
+++ src/com/zgy/ringforu/util/ContactsUtil.java
@@ -122,7 +122,8 @@ public class ContactsUtil {
 				String newNumber = "";
 				do {
 					newNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-					Log.v("", " newNumber =" + newNumber);
+					newNumber = StringUtil.getRidofSpeciall(newNumber);
+//					Log.v("", " newNumber =" + newNumber);
 					if (newNumber.contains(number) || number.contains(newNumber)) {
 						name = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
 						break;
@@ -148,6 +149,7 @@ public class ContactsUtil {
 					String num = "";
 					do {
 						num = cur.getString(cur.getColumnIndex(People.NUMBER));
+						num = StringUtil.getRidofSpeciall(num);
 						if (num.contains(number) || number.contains(num)) {
 							name = cur.getString(cur.getColumnIndex(People.NAME)) + ":" + num;
 							break;
